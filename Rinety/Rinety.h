fa2d67e8e06ce2eb95b8ea71ca5b1331b645b08d
Enrico Secondulfo <enrico.secondulfo85@gmail.com>
2018-01-02 12:56:19 +0100

    Initial Commit

---
+++ Rinety/Rinety.h
@@ -0,0 +1,19 @@
+//
+//  Rinety.h
+//  Rinety
+//
+//  Created by Enrico Secondulfo on 02/01/2018.
+//  Copyright © 2018 splitpix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Rinety.
+FOUNDATION_EXPORT double RinetyVersionNumber;
+
+//! Project version string for Rinety.
+FOUNDATION_EXPORT const unsigned char RinetyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Rinety/PublicHeader.h>
+
+
